c8ea9cf506b8d6d9b279276aaf5683b822116770
Issa Abdi <issa.abdi@kcl.ac.uk>
2023-09-27 10:00:22 +0100

    added exception

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes <= 0:
+        raise ValueError
+    for x in range(0, 100):
+            if x == 0 or x == 1:
+                continue
+            else:
+                for y in range(2, int(x)):
+                    if x % y == 0:
+                        break
+                    elif len(list) >= number_of_primes:
+                        break
+                else:
+                    list.append(x)
     return list
